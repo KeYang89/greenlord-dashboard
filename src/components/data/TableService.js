0facdc571d6acb47a4ebbecfc7321586309f3cc6
Ke Yang <yangkecoy@gmail.com>
2015-12-26 14:51:00 -0600

    相关

--- src/components/data/TableService.js
+++ src/components/data/TableService.js
@@ -10,49 +10,49 @@
   function tableService($q){
     var tableData = [
       {
-        issue: 'Nested views',
+        issue: '响应式设计',
         progress: 100,
         status: 'Done',
         class: 'md-accent'
       },
       {
-        issue: 'Table component',
+        issue: 'Material Design',
         progress: 40,
         status: 'Feedback',
         class: ''
       },
       {
-        issue: 'Dashboard tiles',
+        issue: '卡片化重新设计',
         progress: 100,
         status: 'Done',
         class: 'md-accent'
       },
       {
-        issue: 'Panel widget',
+        issue: '功能修改，使控制面板更简洁',
         progress: 84,
         status: 'In progress',
         class: 'orange'
       },
       {
-        issue: 'Form',
+        issue: '数据可视化',
         progress: 100,
         status: 'Done',
         class: 'md-accent'
       },
       {
-        issue: 'Custom CSS',
+        issue: 'CSS修改',
         progress: 20,
         status: 'Feedback',
         class: ''
       },
       {
-        issue: 'Add backend',
+        issue: 'API接入',
         progress: 1,
         status: 'To do',
         class: 'md-warn'
       },
       {
-        issue: 'Layout with sidebar',
+        issue: '色板选择',
         progress: 100,
         status: 'Done',
         class: 'md-accent'
